952d683f25e59bd0c9669683944fcd18bae76d8f
kledenai <bruno.silva1643@etec.sp.gov.br>
2019-08-30 01:59:27 -0300

    vuee

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Isso requer o compilador
 new Vue({
-    template: '<div>{{ hi }}</div>'
-  })
-  
-  // Isso não requer
-  new Vue({
-    render: h => h('div', this.hi)
-  })
+template: '<div>{{ hi }}</div>'
+})
+
+// Isso não requer
+new Vue({
+render: h => h('div', this.hi)
+})
